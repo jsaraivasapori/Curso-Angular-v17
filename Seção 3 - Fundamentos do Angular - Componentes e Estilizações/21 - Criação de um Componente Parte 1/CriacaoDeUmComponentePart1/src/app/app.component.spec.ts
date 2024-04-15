9722921c5e83326cbdb76a3e43874df36367eb80
João <joaosaraivasapori@gmail.com>
2024-04-15 15:14:33 -0300

    Criação de um componente parte 1

--- Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/CriacaoDeUmComponentePart1/src/app/app.component.spec.ts
+++ Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/CriacaoDeUmComponentePart1/src/app/app.component.spec.ts
@@ -1,18 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CriacaodeumComponenteParte1'`, () => {
+  it(`should have as title 'CriacaoDeUmComponentePart1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CriacaodeumComponenteParte1');
+    expect(app.title).toEqual('CriacaoDeUmComponentePart1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CriacaodeumComponenteParte1');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CriacaoDeUmComponentePart1 app is running!');
   });
 });
