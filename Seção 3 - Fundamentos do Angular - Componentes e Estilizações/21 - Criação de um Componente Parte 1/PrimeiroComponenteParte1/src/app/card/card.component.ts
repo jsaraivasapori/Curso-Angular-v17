209bf8e9efa3a6ca89fac7539dde17645f65a96e
João <joaosaraivasapori@gmail.com>
2024-04-16 13:44:58 -0300

    Aula 29 Encadeamento Opcional na Interpolação

--- Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/card/card.component.ts
+++ Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/card/card.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface IPlano{
+  infos: IInfos
+}
+interface IInfos{
+  tipo:string
+  preco:number
+}
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+  //@ts-ignore
+  plano : any={
+    infos: {
+      tipo:"Simples",
+      preco: 100
+    }
+  }
 
+
+  // tipo:string = 'Complexo'
+  // preco:number = 150
+
+  // getFullPrice() : string{
+  //   setTimeout(()=>{
+  //     this.tipo = 'Simples'
+  //   },4000)
+  //   return `R$ ${this.preco},00 /Mês`
+
+  // }
 }
