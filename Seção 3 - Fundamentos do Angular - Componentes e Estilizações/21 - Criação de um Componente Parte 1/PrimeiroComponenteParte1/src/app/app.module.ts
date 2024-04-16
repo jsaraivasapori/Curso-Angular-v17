e94c55a2dc2e32dca3340cb085302070a713c09e
João Vitor <joaosaraivasapori@gmail.com>
2024-04-15 22:28:37 -0300

    Aula 21

---
+++ Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, //Componentes sempre em declarations. Jamais em imports
+    CardComponent
+  ],
+  imports: [
+    BrowserModule //Caso eu queira usar um componente de outro modulo, e aqui que eu importo o modulo do componente alvo. Sempre importo componente e nunca modulo
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
