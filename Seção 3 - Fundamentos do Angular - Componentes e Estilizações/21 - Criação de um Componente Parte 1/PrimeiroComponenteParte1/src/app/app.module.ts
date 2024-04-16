9ec8879734118654ab598f1ec5fbc612b89c248b
João <joaosaraivasapori@gmail.com>
2024-04-16 15:07:43 -0300

    Aula 39-33

--- Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/app.module.ts
+++ Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { CardsModule } from './cards/cards.module';
 
 
+
 @NgModule({
+  //Componentes sempre em declarations. Jamais em imports
   declarations: [
-    AppComponent, //Componentes sempre em declarations. Jamais em imports
+    AppComponent,
   ],
   imports: [
     BrowserModule, //Caso eu queira usar um componente de outro modulo, e aqui que eu importo o modulo do componente alvo. Sempre importo componente e nunca modulo
