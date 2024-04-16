770e0e810ef65e55fdb921815e4bd8ce1f65c407
João <joaosaraivasapori@gmail.com>
2024-04-16 11:21:25 -0300

    Aula 26

--- Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/app.module.ts
+++ Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/app.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
-import { CardButtonComponent } from './card-button/card-button.component';
-import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
+import { CardsModule } from './cards/cards.module';
+
 
 @NgModule({
   declarations: [
     AppComponent, //Componentes sempre em declarations. Jamais em imports
-    CardComponent, 
-    CardRoxoComponent, CardButtonComponent,CardRoxoButtonComponent
   ],
   imports: [
-    BrowserModule //Caso eu queira usar um componente de outro modulo, e aqui que eu importo o modulo do componente alvo. Sempre importo componente e nunca modulo
+    BrowserModule, //Caso eu queira usar um componente de outro modulo, e aqui que eu importo o modulo do componente alvo. Sempre importo componente e nunca modulo
+    CardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/cards/cards.module.ts
+++ Seção 3 - Fundamentos do Angular - Componentes e Estilizações/21 - Criação de um Componente Parte 1/PrimeiroComponenteParte1/src/app/cards/cards.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardComponent } from '../card/card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardComponent, 
+    CardRoxoComponent, 
+    CardButtonComponent,
+    CardRoxoButtonComponent
+  ],
   imports: [
     CommonModule
+  ],
+
+  exports:[
+    CardComponent, 
+    CardRoxoComponent, 
+    //Componentes filhos nao preciso exportar, pois ja estou exportando o pai deles
+    // CardButtonComponent,
+    // CardRoxoButtonComponent
   ]
 })
 export class CardsModule { }
