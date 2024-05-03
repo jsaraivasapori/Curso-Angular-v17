a368a5518c605c2548ee93ea7043cc2032bb53ac
João Vitor <joaosaraivasapori@gmail.com>
2024-05-02 23:41:57 -0300

    Update Aula 55 Input

--- Seção 4 - Fundamentos do Angular Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card/card.component.ts
+++ Seção 4 - Fundamentos do Angular Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  // input cria uma propriedade e um atributo no seletor do componente
+  @Input({alias:'planTypeAlias', required: true}) planType : string = "" // isso é uma atributo do componente
+   // ao usar o input, o atributio daqui fica igual a propriedade do dom do elemento na componente pai
+  @Input({required:true}) planPrice : number = 0 // so compila esse componente quando esse input tem valor ao ser referenciado no componente pai
+
+
   buttonClicked(valueEmitted : boolean) : void{
     console.log('buttonClicked- componente pai valor:',valueEmitted)
   }
