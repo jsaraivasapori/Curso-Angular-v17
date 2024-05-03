ff6cacc9ce7010df45ef0bc05978879b5769bec3
João Vitor Saraiva Sapori <joaosaraivasapori@gmail.com>
2024-05-02 23:06:29 -0300

    Update Aula 55 - Como funcionar o decorator @Input

--- Seção 4 - Fundamentos do Angular Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card-button/card-button.component.ts
+++ Seção 4 - Fundamentos do Angular Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-button.component.scss']
 })
 export class CardButtonComponent {
+  @Output() buttonClickEmitter = new EventEmitter<boolean>()
+  onButtonClick() : void{
+    console.log("onButtonClick");
+
+    this.buttonClickEmitter.emit(true)
+    
+  }
 
 }
