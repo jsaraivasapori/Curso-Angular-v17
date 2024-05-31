d75c12931e11441387a58dc70dc15b30f6253d1f
João Vitor <joaosaraivasapori@gmail.com>
2024-05-30 21:01:12 -0300

    Seção 4 - Aula 66 - Two Way Data Binding - Update

--- Seção 4 - Fundamentos do Angular Binding, Diretivas, Templates e Decorators/diretiva-estrutural-ngIf/src/app/app.component.ts
+++ Seção 4 - Fundamentos do Angular Binding, Diretivas, Templates e Decorators/diretiva-estrutural-ngIf/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'diretiva-estrutural-ngIf';
+  minhaProp = true;
+  pessoa : any= {
+    status: 1,
+    name: undefined
+  }
+
+  toggleProp(){
+    this.minhaProp = !this.minhaProp 
+    console.log("clicou");
+    
+  }
+
+  isTrusthy(){
+    // return {}
+    // return[]
+    // return true
+    // return 1
+    // return "teste"
+    // return 0
+    // return "false"
+    // return this.pessoa.status
+  }
+
+  isFalsy(){
+    // return false
+    // return 0
+    // return ""
+    // return ''
+    // return null
+    // return undefined
+    // return -0
+    // return NaN
+    // return this.pessoa.name
+    // return this.pessoa.edereco?.rua
+
+  }
+
 }
